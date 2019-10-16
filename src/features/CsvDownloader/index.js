0f4f5bfb4910761e740988c03cb786407683fef6
husseyexplores <husseyexplores@gmail.com>
2019-10-16 20:41:12 +0500

    Restructure files

--- src/features/CsvDownloader/index.js
+++ src/features/CsvDownloader/index.js
@@ -1,12 +1,1 @@
-import React from 'react'
-import PropTypes from 'prop-types'
-
-function CSVDownloader() {
-  return <div>CSVDownloader</div>
-}
-
-CSVDownloader.propTypes = {}
-
-CSVDownloader.defaultProps = {}
-
-export default CSVDownloader
+export { default } from './CsvDownloader'
